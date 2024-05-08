bfdfe445e9d67e5717888896c8982857dc0115b0
jace-d8 <142870592+jace-d8@users.noreply.github.com>
2024-05-07 23:21:17 -0700

    Planning and Pygame setup

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
 # First GitHub Commit!
-print("Hello World")
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((0, 0), pygame.RESIZABLE)
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
+
+# idea
+"""
+The Maze -
+The maze will be a 100 x 100 "grid"(MST?) of set vertices, the walls will be the 
+edges connecting the vertices, the randomization of edges will be done via Kruskals
+Algorithm(kosarju's algor and prims are also candidates but i forgot how they work)
+Solving -
+Should be the easier part of the program; will use recursion or a stack and store each path taken
+in an array to display later
+Graphics - 
+The hard part
+"""
---
+++ maze.py
@@ -0,0 +1,11 @@
+
+class Maze:
+    def __init__(self, maze):
+        self.maze = maze
+
+    def generate_maze(self):
+        print("todo")
+
+    def draw_maze(self):
+        print("todo")
+
