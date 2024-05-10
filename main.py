2a4950473859c19d94a6d029bb1577144c7b4081
jace-d8 <142870592+jace-d8@users.noreply.github.com>
2024-05-10 00:09:06 -0700

    Added a draw grid feature and timer, a constants module, and a 2d array for the Maze

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
-
 import pygame
-from sys import exit
 import maze as m
+import constants as c
+from sys import exit
 
 pygame.init()
 
-screen = pygame.display.set_mode((0, 0), pygame.RESIZABLE)  # Screen size is resizeable so it fits on any display
+# need to figure out how
+# screen = pygame.display.set_mode((0, 0), pygame.RESIZABLE)  # Screen size is resizeable so it fits on any display
+screen = pygame.display.set_mode((1200, 800))
 pygame.display.set_caption("Maze Generator")  # Naming the window to fit my game
 clock = pygame.time.Clock()  # A clock to keep an eye on framerate
-cell = m.Cell(50, 50, 50)
+COLS = int(screen.get_width() / c.SIZE)
+ROWS = int(screen.get_height() / c.SIZE)
+maze = m.Maze(COLS, ROWS)
+# Put pygame in constants and declare screen in there because the screen is const
+# num of cols = screen.get_width() / size
+# num of row = screen.get_height() / size
+# screen size will vary so a cast is needed to ensure no floats are created due to the difference
 
+done = False
 while True:
 
-    screen.fill((255, 255, 255))
-    cell.draw(screen)
+    screen.fill(c.WHITE)
+    pygame.event.pump()
+    maze.draw_maze(screen, COLS, ROWS, done)
+    done = True
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
-    pygame.display.update()
     clock.tick(60)  # the framerate ceiling is 60fps
+    pygame.display.update()
--- maze.py
+++ maze.py
@@ -1,33 +1,51 @@
 import pygame
-
-
-class Maze:
-    def __init__(self):
-        self.maze = {}
-
-    def generate_maze(self, node):
-        self.maze[node] = set()
-
-    def draw_maze(self):
-        print("todo")
+import constants as c
 
 
 class Cell:
-    def __init__(self, x, y, size):  # each cell will need coords to where they are being stored
+    def __init__(self, x, y, visited):  # each cell will need coords to where they are being stored
         self.x = x
         self.y = y
-        self.size = size
+        self.visited = False
+        self.size = c.SIZE
 
     def draw(self, screen):
         # Draw the cell rectangle
-        pygame.draw.rect(screen, (255, 255, 255), (self.x * self.size, self.y * self.size, self.size, self.size))
+        pygame.draw.rect(screen, c.WHITE, (self.x, self.y, self.size, self.size))
         # Draw the cell borders
-        pygame.draw.line(screen, (0, 0, 0), (self.x * self.size, self.y * self.size),((self.x + 1) * self.size, self.y * self.size))
-        pygame.draw.line(screen, (0, 0, 0), ((self.x + 1) * self.size, self.y * self.size), ((self.x + 1) * self.size, (self.y + 1) * self.size))
-        pygame.draw.line(screen, (0, 0, 0), ((self.x + 1) * self.size, (self.y + 1) * self.size),  (self.x * self.size, (self.y + 1) * self.size))
-        pygame.draw.line(screen, (0, 0, 0), (self.x * self.size, (self.y + 1) * self.size),(self.x * self.size, self.y * self.size))
+        pygame.draw.line(screen, c.BLACK, (self.x, self.y),
+                         (self.x + self.size, self.y))  # top
+        pygame.draw.line(screen, c.BLACK, (self.x, self.y),
+                         (self.x, self.y + self.size))  # left side
+        pygame.draw.line(screen, c.BLACK, (self.x + self.size, self.y),
+                         (self.x + self.size, self.y + self.size))  # right side
+        pygame.draw.line(screen, c.BLACK, (self.x, self.y + self.size),
+                         (self.x + self.size, self.y + self.size))  # bottom
 
-        # idea
+
+class Maze:
+    def __init__(self, cols, rows):
+        self.maze = [[Cell(i * c.SIZE, j * c.SIZE, False) for j in range(rows)] for i in range(cols)]
+
+    def generate_maze(self, node):
+        print("todo")
+        """
+        lets use recursion!
+        choose random cell
+        go in a random direction 
+        do this until there is no direction that hasn't been visited
+        by default this will climb back up the class stack until we are back at the starting cell!
+        """
+
+    def draw_maze(self, screen, cols, rows, done):
+        for i in range(cols):
+            for j in range(rows):
+                self.maze[i][j].draw(screen)
+            if not done:
+                pygame.time.delay(25)
+                pygame.display.update()
+        # this is for the original "animation" - it can be done every cell but gets too slow if there are 100+ cells
+
         """
         The Maze -
         The maze will be a graph of set vertices, the walls will be the 
@@ -39,3 +57,9 @@ class Cell:
         Graphics - 
         The hard part
         """
+
+
+"""
+        self.maze = [[Cell() for _ in range(cols)] for _ in range(rows)]
+
+"""
