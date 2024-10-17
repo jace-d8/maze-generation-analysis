31ad7f1d22632659d5bc7be2ce86064c39c1e58e
jace-d8 <142870592+jace-d8@users.noreply.github.com>
2024-10-16 23:55:02 -0700

    Example

--- src/game.py
+++ src/game.py
@@ -11,7 +11,7 @@ class Game:
     def __init__(self):
         # Analyze
         self.analysis = Analysis()
-        self.controls = MazeControls() # example
+        self.controls = MazeControls() # example v2
 
         # Game state
         self.stage = 1
